167d3f82ccc734171fb57727b2d850dad79e4872
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-04-20 14:50:55 -0400

    CompanyListingsViewModel

---
+++ app/src/main/java/com/gmail/orlandroyd/stockmarket/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,11 @@
+package com.gmail.orlandroyd.stockmarket.presentation.company_listings
+
+import com.gmail.orlandroyd.stockmarket.domain.model.CompanyListing
+
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
